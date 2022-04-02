18b9c8a20b7a953749d68cc74cfa34ee8e9659e7
ceckert2194 <ceckert2194@gmail.com>
2022-04-02 15:35:23 -0400

    login page

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web") {
+        alert("Successfully logged in.");
+        location.reload();
+        loginForm.username.value = "";
+        loginForm.password.value = "";
+    } else {
+        loginErrorMsg.style.opacity = 1;
+        loginForm.username.value = "";
+        loginForm.password.value = "";
+    }
+})
